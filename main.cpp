95c7f70f7121e45cd5181828d15afe6f573294c9
ahalshaikh <ahalshaikh@mail.csuchico.edu>
2018-02-26 17:48:14 -0800

    adds the list for items to buy

--- main.cpp
+++ main.cpp
@@ -3,33 +3,56 @@
 #include<string>
 using namespace std;
 
+const int LIST_SIZE = 5;
+
 int main()
 {
-  string list[5]; //array of 5 strings
-  int numItems = 0;
-  char input;
-  string itemName;
-  
-do
-{
-  cout<<"\n==GROCERY LIST MANAGER==";
-  cout<<"\nEnter your choice: ";
-  cout<<"\n (A)dd an item";
-  cout<<"\n (Q)uit";
-  cout<<"\nYour choice (A/Q): ";
-  cin>>input;
-  if(( input == 'a') || ( input == 'A'))
-  {
-  cout<<"What is the item?\n";
-  cin>>itemName;
-  numItems ++;
-  if(numItems >= 6)
-{
-cout<<"You'll need a bigger list!\n";
-}
-  }
-}
-while(( input != 'q') && ( input != 'Q'));
+        string list[LIST_SIZE]; //array of 5 strings
+        int numItems = 0;
+        char input;
+        string itemName;
 
-return 0;
+
+        do
+        {
+                cout<<"\n==GROCERY LIST MANAGER==";
+                cout<<"\nEnter your choice: ";
+                cout<<"\n (A)dd an item";
+                cout<<"\n (Q)uit";
+                cout<<"\nYour choice (A/Q): ";
+                cin>>input;
+
+                if(( input == 'a') || ( input == 'A'))
+                {
+                        cout<<"What is the item?\n";
+                        cin>>itemName;
+                      
+
+                        if(numItems >= 6)
+                        {
+                                cout<<"You'll need a bigger list!\n";
+
+                        }
+
+                        else if (numItems < LIST_SIZE)
+                        {
+
+                                list[numItems] = itemName;
+                        numItems ++;
+                        }
+                }
+
+        }
+
+        while(( input != 'q') && ( input != 'Q'));
+         
+         cout<<"==ITEMS TO BUY=="<<endl;
+
+        for(int index = 0; index < LIST_SIZE; index++)
+        {
+
+        cout<<(index + 1);
+                cout<<" "<<list[index]<<endl;
+        }
+        return 0;
 }
